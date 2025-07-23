2bfef1b2b477e824733f8f8e41e26bb060463b6f
Aliya <itsaliya54@gmail.com>
2025-07-23 23:55:19 +0530

    updated

--- C Language/calculate_area_of_a_square.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-   int side,area;
-   printf("Enter the side: ");
-   scanf("%d",&side);
- area = side*side;
-   printf("Area of the square is: %d",area);
-    return 0;
-}
